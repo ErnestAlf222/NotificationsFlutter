a00ecd2f4bb7e450869e0cbb43b293440573af93
Ernesto Alfonso Sanchez Montiel <ernest.alf45@gmail.com>
2023-07-05 08:58:04 -0600

    App push_app

---
+++ android/app/src/main/kotlin/com/ernestomontiel/push_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ernestomontiel.push_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/presentation/blocs/notifications/notifications_bloc.dart
@@ -0,0 +1,41 @@
+import 'package:equatable/equatable.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:push_app/firebase_options.dart';
+
+part 'notifications_event.dart';
+part 'notifications_state.dart';
+
+class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+  NotificationsBloc() : super( const NotificationsState() ) {
+    on<NotificationsEvent>((event, emit) {
+    });
+  }
+
+
+  static  Future<void> initializeFCM() async {
+     await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  }
+
+
+
+  // Pedir permisos
+  void requestPermission() async {
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: true,
+    provisional: false,
+    sound: true,
+  );
+
+  settings.authorizationStatus;
+
+  }
+}
